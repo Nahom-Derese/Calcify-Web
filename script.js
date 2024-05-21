6497ca6ff7686731b036f31fa66770989f2abf7a
Nahom-Derese <nahomderese@gmail.com>
2024-05-21 13:33:19 +0300

    Update calculator layout and styling

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// Yenu and Yabu Work On the logic of the calculator here
 var display = document.getElementById('display');
-var operand1 = 0;
-var operand2 = null;
-var operator = null;
 
 function inputNumber(num) {
     if (display.value === '0' || display.value === '') {
@@ -20,46 +16,18 @@ function inputDecimal() {
 
 function clearDisplay() {
     display.value = '';
-    operand1 = 0;
-    operand2 = null;
-    operator = null;
 }
 
-function add() {
-    operand1 = display.value;
-    operator = '+';
-    display.value = '';
-}
-
-
-function subtract() {
-    operand1 = display.value;
-    operator = '-';
-    display.value = '';
-}
-
-function multiply() {
-    operand1 = display.value;
-    operator = '*';
-    display.value = '';
-}
-
-function divide() {
-    operand1 = display.value;
-    operator = '/';
-    display.value = '';
+function inputOperator(op) {
+    
+display.value += op;
 }
 
 function calculate() {
-    operand2 = display.value;
-    var result;
     try {
-        result = eval(operand1 + operator + operand2);
-        if (operator === '/' && operand2 === '0') {
-            display.value = 'Error';
-        } else {
-            display.value = result;
-        }
+        
+    var result = eval(display.value);
+        display.value = result;
     } catch (e) {
         display.value = 'Error';
     }
