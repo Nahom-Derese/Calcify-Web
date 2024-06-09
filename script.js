525831599dab17a367ed503dcbd79a11cc4db670
Nahom-Derese <nahomderese@gmail.com>
2024-06-09 09:29:29 +0300

    feature: keyboard input + clear on error

--- script.js
+++ script.js
@@ -1,34 +1,106 @@
-var display = document.getElementById('display');
+// var display = document.getElementById('display');
+
+// function inputNumber(num) {
+//     if (display.value === '0' || display.value === '') {
+//         display.value = num;
+//     } else {
+//         display.value += num;
+//     }
+// }
+
+// function inputDecimal() {
+//     if (display.value.indexOf('.') === -1) {
+//         display.value += '.';
+//     }
+// }
+
+// function clearDisplay() {
+//     display.value = '';
+// }
+
+// function inputOperator(op) {
+    
+// display.value += op;
+// }
+
+// function calculate() {
+//     try {
+        
+//     var result = eval(display.value);
+//         display.value = result;
+//     } catch (e) {
+//         display.value = 'Error';
+//     }
+// }
+
+var display = document.getElementById("display");
+answered = "";
 
 function inputNumber(num) {
-    if (display.value === '0' || display.value === '') {
-        display.value = num;
-    } else {
-        display.value += num;
-    }
+  if (display.value == "Error!" || answered !== "") {
+    display.value = num;
+    answered = "";
+  } else {
+    display.value += num;
+  }
 }
 
 function inputDecimal() {
-    if (display.value.indexOf('.') === -1) {
-        display.value += '.';
-    }
+  if (answered !== "") {
+    answered = "";
+    display.value = "0.";
+  } else if (display.value === "") {
+    display.value += "0.";
+  } else if (display.value.indexOf(".") === -1) {
+    display.value += ".";
+  }
 }
 
 function clearDisplay() {
-    display.value = '';
+  display.value = "";
+  answered = "";
 }
 
 function inputOperator(op) {
-    
-display.value += op;
+  if (display.value !== "") {
+    display.value += op;
+    if (answered !== "") {
+      answered = "";
+    }
+  }
 }
 
 function calculate() {
-    try {
-        
+  try {
     var result = eval(display.value);
-        display.value = result;
-    } catch (e) {
-        display.value = 'Error';
+    answered = result;
+    if (result === Infinity || result === -Infinity) {
+      throw new Error("Error!");
     }
+
+    display.value = result;
+  } catch (e) {
+    display.value = "Error!";
+  }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  function handleKeyPress(event) {
+    const key = event.key;
+    if (['1','2','3','4','5','6','7','8','9','0'].includes(key)) {
+      inputNumber(Number(key));
+    } else if (["+", "-", "*", "/"].includes(key)) {
+      inputOperator(key);
+    } else if (key === "Enter" || key === "=") {
+      calculate();
+    } else if (key === "Escape") {
+      clearDisplay();
+    } else if (key == "Backspace") {
+      backspace();
+    } else if (key == ".") {
+      inputDecimal();
+    }
+  }
+
+  document.addEventListener('keydown', handleKeyPress);
+});
